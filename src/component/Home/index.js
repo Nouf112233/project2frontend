12c518cebefaf0bfb4c687238a0a6de08dfc27da
Nouf112233 <Nouf.Mohamed.elq.js@tuwaiq.edu.sa>
2021-11-22 17:02:52 +0300

    add

--- src/component/Home/index.js
+++ src/component/Home/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Product from '../Product'
+import Slider from '../Slider'
 
 import "./style.css"
 
 function Home() {
     return (
         <div className="home">
+            <Slider />
             <div className="home-container">
                 <img className="home-img" src="" alt="image" />
                 <div className="home-row">
