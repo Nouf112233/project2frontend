12efd1122bf577c658d6c7c94a0e5ea3e13472b9
Nouf112233 <Nouf.Mohamed.elq.js@tuwaiq.edu.sa>
2021-11-25 08:20:18 +0300

    add

--- src/component/Basket/index.js
+++ src/component/Basket/index.js
@@ -1,38 +1,52 @@
 import React, { useEffect, useState } from "react";
+import Header from "../Header";
+import axios from "axios";
 // import CheckoutProduct from "../checkoutProduct";
 import { AiOutlineStar } from "react-icons/ai";
 import Subtotal from "../Subtotal";
 
 function Basket() {
-    const [cartsId,setCartsId]=useState([]);
+
   const [basket, setBasket] = useState([]);
+  const [email,setEmail]=useState("")
 
-  const getBasketProduct=()=>{
-    const user=JSON.parse(sessionStorage.getItem("user"));
+  const getBasketProduct= async()=>{
+    let user=JSON.parse(sessionStorage.getItem("user"));
+    let carts=user.cart;
+
+    console.log("user",user);
+
     if(user){
-        const cart=user.cart;
-        cart.map(item=>{
-            
-        })
-        setBasket(cart)
+        const cart=carts.map(item=>{
+                 console.log("item",item);
+                  return  item;
+                //   axios.get(
+                //     `http://localhost:5000/product/id/${item}`
+         } );
+                //   return pro;
+                console.log("cart",cart)   
+                setBasket(cart)    
+        }  
     }
-  }
+  
 
-  const removeCart = (id) => {
+  const removeCart = (id,i) => {
         
 }
+
+//   useEffect(() => {
+//     getBasketProduct();
+//   }, []);
 
   useEffect(() => {
     getBasketProduct();
   }, []);
 
-  useEffect(() => {
-    getBasketProduct();
-  }, [basket]);
-
   return (
+      <>
+      <Header />
     <div className="checkout">
-      {/* <div className="checkout-left">
+      <div className="checkout-left">
         <img
           className="checkout-left"
           src="https://i.pinimg.com/236x/30/45/d6/3045d6e4080109bd6b6645cf104ea94b.jpg"
@@ -40,7 +54,7 @@ function Basket() {
         />
         <div>
           <h2 className="checkout-title">Your Shopping Basket</h2>
-          {basket && (basket.map(item=>{
+          {/* {basket && (basket.map((item,i)=>{
               return(
                     <div className="checkoutProduct">
                     <img src={item.image[0]} className="checkoutProduct-image" />
@@ -56,19 +70,20 @@ function Basket() {
                             {Array(item.rating).fill.map((_,i)=>
                             <p><AiOutlineStar /></p>)}
                         </div>
-                        <button onClick={()=>removeCart(item._id)}>
+                        <button onClick={()=>removeCart(item._id,i)}>
                             Remove from basket
                         </button>
                     </div>
                     
                 </div>
-          )}))}
+          )}))} */}
         </div>
       </div>
       <div className="checkout-right">
         <Subtotal />
-      </div> */}
+      </div>
     </div>
+    </>
   );
 }
 
